3d16941150359c424b545e179b729d70d97798f2
John <serradillamate.alejandro@gmail.com>
2016-11-30 11:54:46 +0100

    sound implemented

--- app/src/main/java/com/example/ipsenspiegel/mymultimediamanager/MainActivity.java
+++ app/src/main/java/com/example/ipsenspiegel/mymultimediamanager/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.ipsenspiegel.mymultimediamanager;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.IOException;
 
-    private static final String TAG_MAIN_ACTIVITY = MainActivity.class.getSimpleName();
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnPreparedListener {
 
+    private static final String TAG_MAIN_ACTIVITY = "Main_Activity";
+    private MediaPlayer mPlayer;
+    private Button play_Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button play_Btn = (Button) this.findViewById(R.id.btnPlayTrack);
+
+        this.play_Btn = (Button) this.findViewById(R.id.btnPlayTrack);
         play_Btn.setOnClickListener(this);
         final Button stop_Btn = (Button) this.findViewById(R.id.btnStopTrack);
         stop_Btn.setOnClickListener(this);
@@ -24,17 +30,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         final Button playInService_Btn = (Button) this.findViewById(R.id.btnPlaySoundInService);
         playInService_Btn.setOnClickListener(this);
 
+        this.mPlayer = MediaPlayer.create(this, R.raw.bensoundbrazilsamba);
     }
 
     @Override
     public void onClick(View whichView) {
         if (whichView.getId() == R.id.btnPlayTrack) {
 
-        } else if (whichView.getId() == R.id.btnStopTrack) {
+            if (this.mPlayer.isPlaying()) {
+                Log.i(MainActivity.TAG_MAIN_ACTIVITY, "Track stopped");
+                this.mPlayer.stop();
+                this.play_Btn.setText("Start");
+            }
+            else{
+                Log.d(MainActivity.TAG_MAIN_ACTIVITY, "Playing track");
+                this.mPlayer.start();
+                this.play_Btn.setText("Pause");
+            }
 
+        } else if (whichView.getId() == R.id.btnStopTrack) {
+            if (this.mPlayer.isPlaying()) {
+                this.mPlayer.stop();
+            }
         } else if (whichView.getId() == R.id.btnPlaySound) {
+
 
         } else if (whichView.getId() == R.id.btnPlaySoundInService) {
         }
     }
+
+    
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
 }
+
+
